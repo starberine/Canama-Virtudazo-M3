164b4ec09cbc897784151f8022f898dbb2171a23
Arin <kathcanama@gmail.com>
2025-03-14 05:38:19 +0800

    Moving Platform

---
+++ Luminaries/Assets/Scripts/MovingPlatformCollider.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingPlatformCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
